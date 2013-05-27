2296ed7085c6f215899a5e615ec7d7bf8f600fcd
maxime.sauvage.14@gmail.com <maxime.sauvage.14@gmail.com@ea0d081e-89d9-9a54-84d9-ca2cdb8b9166>
2013-05-27 12:16:46 +0000

    ok

--- trunk/client/testClient.py
+++ trunk/client/testClient.py
@@ -21,6 +21,16 @@ except IOError: exit(1)
 # # play the music
 music.play()
 
+def gereKey():
+    if sf.Keyboard.is_key_pressed(sf.Keyboard.LEFT):
+        print "gauche"
+    elif sf.Keyboard.is_key_pressed(sf.Keyboard.RIGHT):
+        print "droite"
+    elif sf.Keyboard.is_key_pressed(sf.Keyboard.UP):
+        print "haut"
+    elif sf.Keyboard.is_key_pressed(sf.Keyboard.DOWN):
+        print "bas"
+
 # start the game loop
 # sf.sleep(sf.seconds(5))
 while window.is_open:
@@ -31,6 +41,7 @@ while window.is_open:
 	    window.close()
         if type(event) is sf.KeyEvent and event.code is sf.Keyboard.ESCAPE:
             window.close()
+        gereKey()
     window.clear() # clear screen
     window.draw(sprite) # draw the sprite
     # window.draw(text) # draw the string
