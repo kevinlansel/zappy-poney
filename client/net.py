b516beb48405e8bd132d8bddd7c15a04073c0c81
maxime.sauvage.14@gmail.com <maxime.sauvage.14@gmail.com@ea0d081e-89d9-9a54-84d9-ca2cdb8b9166>
2013-06-14 09:35:48 +0000

    ok

---
+++ client/net.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+import socket
+
+class   Net:
+    def __init__(self):
+        self._host = "localhost"
+        self._port = 0
+        self._team = 0
+        self._kill = 0
+        self._sock = 0
+
+    def initConnexion():
+        self._sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+	try:
+	    self._sock.connect((self._host, self._port))
+	except socket.error:
+	    exit()
+	print "Connexion etablie avec le serveur"
+	query = "Connexion d'un nouveau client"
+	sock.send(query)
