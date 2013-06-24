ba157a42b5dbe919d207c340ace3b87238fa4e58
guillaume.duez.epitech@gmail.com <guillaume.duez.epitech@gmail.com@ea0d081e-89d9-9a54-84d9-ca2cdb8b9166>
2013-06-24 13:56:44 +0000

    pl

---
+++ serveur/task_time_client.c
@@ -0,0 +1,26 @@
+/*
+** task_time_client.c for  in /home/duez/zappy-poney/serveur
+** 
+** Made by guillaume duez
+** Login   <duez_a@epitech.net>
+** 
+** Started on  Mon Jun 24 15:22:19 2013 guillaume duez
+** Last update Mon Jun 24 15:52:21 2013 guillaume duez
+*/
+
+#include	"serveur.h"
+
+double		get_time_client(t_client *client, int action)
+{
+  struct timeb   tp;
+  double         time;
+  double         milli;
+
+  ftime(&tp);
+  milli =  (double)tp.millitm / 1000;
+  time = (double)tp.time + milli;
+  if (time < client->action)
+    time = client->action;
+  client->action = time + (action / client->time);
+  return  client->action;
+}
