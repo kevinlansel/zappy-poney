2521519af9da7ac30ba059c4eb6270b99cb430ac
guillaume.duez.epitech@gmail.com <guillaume.duez.epitech@gmail.com@ea0d081e-89d9-9a54-84d9-ca2cdb8b9166>
2013-07-11 12:20:53 +0000

    pl

--- trunk/serveur/fork.c
+++ trunk/serveur/fork.c
@@ -5,7 +5,7 @@
 ** Login   <duez_a@epitech.net>
 ** 
 ** Started on  Mon Jul  8 15:37:27 2013 guillaume duez
-** Last update Thu Jul 11 09:38:27 2013 florian dewulf
+** Last update Thu Jul 11 14:20:29 2013 guillaume duez
 */
 
 #include	"serveur.h"
@@ -26,7 +26,7 @@ void		fork_egg(t_msg *msg, t_client *client, t_map **map)
   sub_food(msg, client, "ok\n");
   msg->time = get_time_client(client, 42);
   new = xmalloc(sizeof(t_client));
-  new->team = team;
+  new->team = client->team;
   new->id = give_id_egg();
   new->level = client->id;
   new->end = 0;
